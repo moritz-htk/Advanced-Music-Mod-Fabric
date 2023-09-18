4824bfec4ae0b9ffa043b450bb0da1f7006f7ed5
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-09-18 15:09:05 +0200

    Initial commit

---
+++ src/main/java/net/moritz_htk/advancedmusicmod/AdvancedMusicMod.java
@@ -0,0 +1,14 @@
+package net.moritz_htk.advancedmusicmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdvancedMusicMod implements ModInitializer {
+	public static final String MOD_ID = "advancedmusicmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
