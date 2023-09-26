35ef10ba119096e8202504555df632ae7e4175ce
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-09-26 18:17:13 +0200

    Updated to Minecraft 1.20.2

--- src/main/java/net/moritz_htk/advancedmusicmod/item/AMMItemGroup.java
+++ src/main/java/net/moritz_htk/advancedmusicmod/item/AMMItemGroup.java
@@ -3,15 +3,19 @@ package net.moritz_htk.advancedmusicmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.moritz_htk.advancedmusicmod.AdvancedMusicMod;
 
 public class AMMItemGroup {
-    static ItemGroup ADVANCED_MUSIC_MOD_TAB;
+    public static final RegistryKey<ItemGroup> ADVANCED_MUSIC_MOD_TAB = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(AdvancedMusicMod.MOD_ID, "advanced_music_mod_tab"));
 
     public static void registerItemGroups() {
-        ADVANCED_MUSIC_MOD_TAB = FabricItemGroup.builder(new Identifier(AdvancedMusicMod.MOD_ID, "advanced_music_mod_tab")).displayName(Text.literal("Advanced Music Mod")).icon(() -> new ItemStack(AMMItems.MUSIC_DISC_AUGUST)).build();
+        Registry.register(Registries.ITEM_GROUP, ADVANCED_MUSIC_MOD_TAB, FabricItemGroup.builder().displayName(Text.literal("Advanced Music Mod")).icon(() -> new ItemStack(AMMItems.MUSIC_DISC_AUGUST)).build());
         AdvancedMusicMod.LOGGER.info("Registering ItemGroup for " + AdvancedMusicMod.MOD_ID);
     }
 }
